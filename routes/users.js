68244cdafe5dc9f1fc4bc74c74c5f95166d9106b
Bharatkhatri <bharatkhatri351@gmail.com>
2021-05-15 23:56:34 +0530

    Added Timer to dashboard page and fix some spacing

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,43 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 var game = [
   {
-    name : 'abcd',
-    noOfPlayers : '2',
-    rounds : '1',
-    time : '22-09-1999 22:01'
-  }
+    name: "abcd",
+    noOfPlayers: "2",
+    rounds: "1",
+    time: "22-09-1999 22:01",
+  },
 ];
 
 /* GET users listing. */
 
-router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard');
+router.get("/dashboard", function (req, res, next) {
+  res.render("dashboard");
 });
 
 //create game
-router.post('/create', function(req, res, next) {
+router.post("/create", function (req, res, next) {
   const newGame = {
-    "name" : req.body.name,
-    "noOfPlayers"  : req.body.noOfPlayers,
-    "rounds"  : req.body.rounds,
-    "time" : req.body.time
+    name: req.body.name,
+    noOfPlayers: req.body.noOfPlayers,
+    rounds: req.body.rounds,
+    time: req.body.time,
   };
-  game.push(newGame); 
+  game.push(newGame);
   console.log(game);
   //res.json(game);
   //res.render('dashboard');
 });
 
-router.get('/viewGame', function(req, res, next){
-  res.render('viewGame',{game:game});
+router.get("/viewGame", function (req, res, next) {
+  res.render("viewGame", { game: game });
 });
 
+router.post("/joinGame", function (req, res, next) {});
 
-router.post('/joinGame', function(req, res, next){
-
+router.get("/dashboard1", function (req, res, next) {
+  res.render("dashboard1", { joinedGame: false });
 });
-
-router.get('/dashboard1', function(req, res, next) {
-  res.render('dashboard1',{joinedGame:true});
-});
-
-
-
-
-
-
 
 module.exports = router;
